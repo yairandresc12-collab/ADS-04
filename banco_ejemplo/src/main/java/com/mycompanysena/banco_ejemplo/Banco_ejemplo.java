295c7a7ad65b5973eee12e726777cea4811b2710
yairandresc12-collab <yairandresc12@gmail.com>
2025-12-01 14:11:21 -0500

    Add files via upload

---
+++ banco_ejemplo/src/main/java/com/mycompanysena/banco_ejemplo/Banco_ejemplo.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompanysena.banco_ejemplo;
+
+
+public class Banco_ejemplo {
+
+    public static void main(String[] args) {
+        cajero cajero = new cajero ();
+        cajero.Cajeroautomatico();
+       
+            
+        
+    }
+}
